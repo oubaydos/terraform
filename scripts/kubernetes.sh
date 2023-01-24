3565bb669997c2eb133f7e883714440f7183310f
obaydah bouifadene <obaydah.bouifadene@um5r.ac.ma>
2023-01-24 01:12:49 +0100

    changed script

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -1,32 +1,47 @@
 #! /bin/bash
-sudo touch ~/output.txt
+sudo touch /home/oubay/output.txt
 sudo apt update -y
-echo "updated" >>~/output.txt
+echo "updated" >>/home/oubay/output.txt
 sudo apt upgrade -y
-echo "upgraded" >>~/output.txt
+echo "upgraded" >>/home/oubay/output.txt
 sudo apt install -y docker.io
-echo "installed docker" >>~/output.txt
+echo "installed docker" >>/home/oubay/output.txt
 sudo systemctl start docker
-echo "started docker" >>~/output.txt
+echo "started docker" >>/home/oubay/output.txt
 sudo systemctl enable docker
-echo/ "enabled docker" >>~/output.txt
+echo/ "enabled docker" >>/home/oubay/output.txt
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-echo "downloaded minikube" >>~/output.txt
+echo "downloaded minikube" >>/home/oubay/output.txt
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
-echo "installed minikube" >>~/output.txt
+echo "installed minikube" >>/home/oubay/output.txt
 sudo ln -s $(which minikube) /usr/local/bin/kubectl
-echo "linked kubectl" >>~/output.txt
+echo "linked kubectl" >>/home/oubay/output.txt
+#
+#### the part bellow is not successfully starting minikube
+#sudo -u oubay -H sh -c 'sudo usermod -aG docker oubay && newgrp docker'
+#echo "added user oubay to docker group" >>/home/oubay/output.txt
+sudo chmod 666 /var/run/docker.sock;
+echo "added permissions to docker socker" >> /home/oubay/output.txt 2>&1;
+sudo -u oubay -H sh -c 'minikube start';
+echo "started minikube" >>/home/oubay/output.txt 2>&1;
 
-### the part bellow is not successfully starting minikube
-sudo -i -u oubay bash <<EOF
-sudo usermod -aG docker oubay && newgrp docker;
-echo "added user oubay to docker group" >> ~/output.txt;
-minikube start;
-sudo echo "started minikube" >> ~/output.txt;
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3;
-sudo bash ./get_helm.sh;
-sudo echo "installed helm" >> ~/output.txt;
-helm repo add bitnami https://charts.bitnami.com/bitnami;
-sudo echo "added bitnami repo" >> ~/output.txt;
-sudo echo "installed kafka" >> ~/output.txt;
-EOF
+sudo -u oubay -H sh -c 'curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3';
+sudo -u oubay -H sh -c 'sudo bash ./get_helm.sh';
+echo "installed helm" >>/home/oubay/output.txt 2>&1;
+sudo -u oubay -H sh -c 'helm repo add bitnami https://charts.bitnami.com/bitnami';
+echo "added bitnami repo" >>/home/oubay/output.txt 2>&1;
+sudo -u oubay -H sh -c 'helm install my-release bitnami/kafka',
+echo "installed kafka" >>/home/oubay/output.txt 2>&1;
+
+#sudo -i -u oubay bash <<EOF
+#sudo usermod -aG docker oubay && newgrp docker;
+#echo "added user oubay to docker group" >> /home/oubay/output.txt;
+#minikube start;
+#sudo echo "started minikube" >> /home/oubay/output.txt;
+#curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3;
+#sudo bash ./get_helm.sh;
+#sudo echo "installed helm" >> /home/oubay/output.txt;
+#helm repo add bitnami https://charts.bitnami.com/bitnami;
+#sudo echo "added bitnami repo" >> /home/oubay/output.txt;
+#sudo echo "installed kafka" >> /home/oubay/output.txt;
+#EOF
